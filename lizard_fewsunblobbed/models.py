55334024ac0188c9cb0c0e7f1e3d13e884b284c8
Reinout <Reinout@476a50c0-da5d-5340-8c75-4ad663a144f1>
2010-04-08 12:53:12 +0000

    pep8

--- lizard_fewsunblobbed/models.py
+++ lizard_fewsunblobbed/models.py
@@ -2,22 +2,24 @@ from django.db import models
 from composite_pk import composite
 from treebeard.al_tree import AL_Node
 
+
 class Filter(AL_Node):
     fkey = models.IntegerField(primary_key=True)
-    id = models.TextField(unique=True) 
-    name = models.TextField(blank=True) 
-    description = models.TextField(blank=True) 
+    id = models.TextField(unique=True)
+    name = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     issubfilter = models.BooleanField()
-    parent = models.ForeignKey('Filter', null=True, blank=True, db_column='parentfkey')
+    parent = models.ForeignKey('Filter', null=True, blank=True,
+                               db_column='parentfkey')
     isendnode = models.BooleanField()
     node_order_by = ['name']
-
 
     class Meta:
         db_table = u'filter'
 
     def __unicode__(self):
-        return '%d %s (%s)'%(self.fkey, self.id, self.name)
+        return '%d %s (%s)' % (self.fkey, self.id, self.name)
+
 
 class Location(models.Model):
     lkey = models.IntegerField(primary_key=True)
@@ -25,18 +27,20 @@ class Location(models.Model):
     name = models.TextField(blank=True)
     parentid = models.TextField(blank=True)
     description = models.TextField(blank=True)
-    shortname = models.TextField(blank=True) 
+    shortname = models.TextField(blank=True)
     tooltiptext = models.TextField(blank=True)
     x = models.FloatField(blank=True)
     y = models.FloatField(blank=True)
     z = models.FloatField(blank=True)
     longitude = models.FloatField(blank=True)
-    latitude = models.FloatField(blank=True)    
+    latitude = models.FloatField(blank=True)
+
     class Meta:
         db_table = u'location'
 
     def __unicode__(self):
-        return '%d %s (%s)'%(self.lkey, self.id, self.name)
+        return '%d %s (%s)' % (self.lkey, self.id, self.name)
+
 
 class Parameter(models.Model):
     pkey = models.IntegerField(primary_key=True)
@@ -46,11 +50,13 @@ class Parameter(models.Model):
     unit = models.TextField(blank=True)
     parametertype = models.TextField(blank=True)
     parametergroup = models.TextField(blank=True)
+
     class Meta:
         db_table = u'parameter'
 
     def __unicode__(self):
-        return '%d %s (%s)'%(self.pkey, self.id, self.name)
+        return '%d %s (%s)' % (self.pkey, self.id, self.name)
+
 
 class Timeserie(models.Model):
     tkey = models.IntegerField(primary_key=True)
@@ -59,11 +65,14 @@ class Timeserie(models.Model):
     filterkey = models.ForeignKey(Filter, db_column='filterkey')
     locationkey = models.ForeignKey(Location, db_column='locationkey')
     parameterkey = models.ForeignKey(Parameter, db_column='parameterkey')
+
     class Meta:
         db_table = u'timeserie'
 
     def __unicode__(self):
-        return '%s (%s::%s::%s)'%(self.moduleinstanceid, self.filterkey, self.locationkey, self.parameterkey)
+        return '%s (%s::%s::%s)' % (self.moduleinstanceid, self.filterkey,
+                                    self.locationkey, self.parameterkey)
+
 
 class Timeseriedata(composite.CompositePKModel):
     tkey = models.ForeignKey(Timeserie, primary_key=True, db_column='tkey')
@@ -72,10 +81,9 @@ class Timeseriedata(composite.CompositePKModel):
     tsd_flag = models.IntegerField(blank=True)
     tsd_detection = models.BooleanField()
     tsd_comments = models.TextField(blank=True)
+
     class Meta:
         db_table = u'timeseriedata'
 
     def __unicode__(self):
-        return '%s %s %s'%(self.tkey, self.tsd_time, self.tsd_value)
-
-
+        return '%s %s %s' % (self.tkey, self.tsd_time, self.tsd_value)
