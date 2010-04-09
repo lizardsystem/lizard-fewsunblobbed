6e80479395b798ad884bfc8591af0b09ce4dcdca
JanMaarten <JanMaarten@476a50c0-da5d-5340-8c75-4ad663a144f1>
2010-04-09 12:54:48 +0000

    Fixing the dump_bulk method to get the tree dumped nicely.

--- lizard_fewsunblobbed/models.py
+++ lizard_fewsunblobbed/models.py
@@ -1,24 +1,72 @@
+from django.core import serializers
 from django.db import models
 from composite_pk import composite
 from treebeard.al_tree import AL_Node
 
 
 class Filter(AL_Node):
-    fkey = models.IntegerField(primary_key=True)
-    id = models.TextField(unique=True)
+    # use 'id' and not fkey as treebeard needs this name (is hardcoded)
+    id  = models.IntegerField(primary_key=True, db_column='fkey') 
+    # since 'id' is already used us another name
+    fews_id = models.TextField(unique=True, db_column='id')
     name = models.TextField(blank=True)
     description = models.TextField(blank=True)
     issubfilter = models.BooleanField()
+    # use 'parent' and not parentfkey as treebeard needs this name (is hardcoded)
     parent = models.ForeignKey('Filter', null=True, blank=True,
                                db_column='parentfkey')
     isendnode = models.BooleanField()
-    node_order_by = ['name']
+    node_order_by = ['name',]
 
     class Meta:
         db_table = u'filter'
 
     def __unicode__(self):
         return '%d %s (%s)' % (self.fkey, self.id, self.name)
+
+    # This method is overriden from the class AL_Node in al_tree.py
+    # from the django-treebeard application
+    # The method fixes checks first if the field sib_order exist before deleting,
+    # which the original version doesn't do (which causes a bug). 
+    @classmethod
+    def dump_bulk(cls, parent=None, keep_ids=True):
+        """
+        Dumps a tree branch to a python data structure.
+
+        See: :meth:`treebeard.Node.dump_bulk`
+        """
+
+        # not really a queryset, but it works
+        qset = cls.get_tree(parent)
+
+        ret, lnk = [], {}
+        pos = 0
+        for pyobj in serializers.serialize('python', qset):
+            node = qset[pos]
+            depth = node.get_depth()
+            # django's serializer stores the attributes in 'fields'
+            fields = pyobj['fields']
+            del fields['parent']
+            if 'sib_order' in fields:
+                del fields['sib_order']
+            if 'id' in fields:
+                del fields['id']
+
+            newobj = {'data': fields}
+            if keep_ids:
+                newobj['id'] = pyobj['pk']
+
+            if (not parent and depth == 1) or \
+                    (parent and depth == parent.get_depth()):
+                ret.append(newobj)
+            else:
+                parentobj = lnk[node.parent_id]
+                if 'children' not in parentobj:
+                    parentobj['children'] = []
+                parentobj['children'].append(newobj)
+            lnk[node.id] = newobj
+            pos += 1
+        return ret
 
 
 class Location(models.Model):
