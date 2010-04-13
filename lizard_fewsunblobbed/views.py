52f5ddcc30d892e7c4fdd18c4cc977abbe35fc95
JanMaarten <JanMaarten@476a50c0-da5d-5340-8c75-4ad663a144f1>
2010-04-13 14:59:36 +0000

    Improving the tree visualization

--- lizard_fewsunblobbed/views.py
+++ lizard_fewsunblobbed/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from lizard_fewsunblobbed.models import Filter
 
-def fews_browser(request):    
+def fews_filter_tree(request, template='lizard_fewsunblobbed/filter_tree.html'):    
     tree = Filter.dump_bulk()    
-    return render_to_response('filter_tree.html', 
-                              {"tree": tree})
+    return render_to_response(template,
+                              {"tree": tree},
+                              context_instance=RequestContext(request))
 
