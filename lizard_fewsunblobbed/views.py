96b4f04b54f0add79bcef1f3bbe2bc10ec6b08bd
JanMaarten <JanMaarten@476a50c0-da5d-5340-8c75-4ad663a144f1>
2010-04-13 12:14:13 +0000

    added templates for visualizing the filter tree

--- lizard_fewsunblobbed/views.py
+++ lizard_fewsunblobbed/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from lizard_fewsunblobbed.models import Filter
+
+def fews_browser(request):    
+    tree = Filter.dump_bulk()    
+    return render_to_response('filter_tree.html', 
+                              {"tree": tree})
+
