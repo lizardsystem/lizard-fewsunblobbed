d7cc41bf1bb28eca068d3a1e57d2e848bd4eb489
JanMaarten <JanMaarten@476a50c0-da5d-5340-8c75-4ad663a144f1>
2010-04-14 06:10:03 +0000

    Moving templates to right folder and deleting a template which was located twice in the repository

--- lizard_fewsunblobbed/views.py
+++ lizard_fewsunblobbed/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from lizard_fewsunblobbed.models import Filter
+from lizard_fewsunblobbed.models import Parameter
+from lizard_fewsunblobbed.models import Timeserie
 
 def fews_filter_tree(request, template='lizard_fewsunblobbed/filter_tree.html'):    
     tree = Filter.dump_bulk()    
@@ -8,3 +10,11 @@ def fews_filter_tree(request, template='lizard_fewsunblobbed/filter_tree.html'):
                               {"tree": tree},
                               context_instance=RequestContext(request))
 
+def fews_parameter_tree(request, filterkey=90, locationkey=37551, template='lizard_fewsunblobbed/parameter_tree.html'):
+    filtered_timeseries = Timeserie.objects.filter(filterkey=filterkey, locationkey=locationkey)
+    parameters = [ts.parameterkey for ts in filtered_timeseries]
+    return render_to_response(template,
+                             {"parameters": parameters})
+
+
+    
