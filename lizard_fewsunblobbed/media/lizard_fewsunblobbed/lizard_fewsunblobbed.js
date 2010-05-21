512f34e8aaf6600fd5f0060e0a505ea5ff8097b4
Jack <Jack@476a50c0-da5d-5340-8c75-4ad663a144f1>
2010-05-21 09:28:53 +0000

    separated js fews unblobbed

--- lizard_fewsunblobbed/media/lizard_fewsunblobbed/lizard_fewsunblobbed.js
+++ lizard_fewsunblobbed/media/lizard_fewsunblobbed/lizard_fewsunblobbed.js
@@ -1,85 +1,80 @@
 /* javascript files for fews unblobbed: handling filter trees and parameters */
 
-applyTreeView = function() {
-  $("#navigation").treeview({
-    //persist: "cookie",
-    //cookieId: "fewsbrowser",
-    collapsed: true
-  });
-};
+function applyTreeView() {
+    $("#navigation").treeview({
+        collapsed: true
+    });
+}
 
 // (Re-)bind (possible) workspaceitems to the draggingmechanism.
-applyDragging = function() {
-  // Parameters are draggable to workspaces.
-  $(".workspace-acceptable").draggable({
-    scroll: 'false',
-    cursor: 'move',
-    helper: 'clone',
-    appendTo: 'body',
-    revert: 'true',
-    placeholder: 'ui-sortable-placeholder'
-  });
+function applyDragging() {
+    // Parameters are draggable to workspaces.
+    $(".workspace-acceptable").draggable({
+        scroll: 'false',
+        cursor: 'move',
+        helper: 'clone',
+        appendTo: 'body',
+        revert: 'true',
+        placeholder: 'ui-sortable-placeholder'
+    });
 
-  // Clicking a workspace-acceptable shows it in the 'temp' workspace.
-  $(".workspace-acceptable").bind({
-    click: function(event) {
-      $(".workspace-acceptable").removeClass("selected");
-      $(this).addClass("selected");
-      var name = $(this).attr("name");
-      var layer_method = $(this).attr("layer_method");
-      var layer_method_json = $(this).attr("layer_method_json");
-        var url_add_item_temp = $("a.url-lizard-map-session-workspace-add-item-temp").attr("href");
-      $.post(url_add_item_temp,
-             { name: name,
-               layer_method: layer_method,
-               layer_method_json: layer_method_json
-             },
-             function(workspace_id){
-               updateLayer(workspace_id);
-             }
-            );
-      stretchOneSidebarBox();
-    }
-  });
-};
+    // Clicking a workspace-acceptable shows it in the 'temp' workspace.
+    $(".workspace-acceptable").bind({
+        click: function(event) {
+            $(".workspace-acceptable").removeClass("selected");
+            $(this).addClass("selected");
+            var name = $(this).attr("name");
+            var layer_method = $(this).attr("layer_method");
+            var layer_method_json = $(this).attr("layer_method_json");
+            var url_add_item_temp = $("a.url-lizard-map-session-workspace-add-item-temp").attr("href");
+            $.post(url_add_item_temp,
+                   { name: name,
+                     layer_method: layer_method,
+                     layer_method_json: layer_method_json
+                   },
+                   function(workspace_id){
+                       updateLayer(workspace_id);
+                   }
+                  );
+            stretchOneSidebarBox();
+        }
+    });
+}
 
-  applyClickingOnFilter = function(){
-  // Clicking a filter highlights it and loads its parameters.
-  $(".file").click(function(){
-    var id = $(this).attr('id');
-      var url_fews_parameter_tree_base = $("a.url-fews-parameter-tree-base").attr("href");
-    $("#navigation li.selected").removeClass("selected");
-    $(this).parent("li").addClass("selected");
-    $("#parametertree").load(
-      url_fews_parameter_tree_base + id + "/37551/",
-      function() {
-        applyDragging();
-      });
-    filterParameterAccordeon.click(1);
-  });
-};
+function applyClickingOnFilter() {
+    // Clicking a filter highlights it and loads its parameters.
+    $(".file").click(function(){
+        var id = $(this).attr('id');
+        var url_fews_parameter_tree_base = $("a.url-fews-parameter-tree-base").attr("href");
+        $("#navigation li.selected").removeClass("selected");
+        $(this).parent("li").addClass("selected");
+        $("#parametertree").load(
+            url_fews_parameter_tree_base + id + "/37551/",
+            function() {
+                applyDragging();
+            });
+        filterParameterAccordeon.click(1);
+    });
+}
 
 // load the tree. !if the page is loaded, run this function!
 // uses a.url-fews-filter-tree
-loadFilterTree = function(){
+function loadFilterTree() {
     var url_fews_filter_tree = $("a.url-fews-filter-tree").attr("href");
-    $.ajax({
-        url: url_fews_filter_tree,
-        context: document.body,
-        success: function(data){
-            $("#navigation").replaceWith(data);
-            applyTreeView();
-            applyClickingOnFilter();
-            stretchOneSidebarBox();
-        }   
-    });
-}; 
+    if (url_fews_filter_tree !== undefined) {
+        $.ajax({
+            url: url_fews_filter_tree,
+            context: document.body,
+            success: function(data){
+                $("#navigation").replaceWith(data);
+                applyTreeView();
+                applyClickingOnFilter();
+                stretchOneSidebarBox();
+            }   
+        });
+    }
+} 
 
 // Set up filter tree if page is loaded 
-
-// todo: hij zegt wel dat hij
-// klaar is, maar dat is niet zo... de $("...").href("..") wil nog
-// niet 
-
-//$(document).ready(loadFilterTree());
+$(document).ready(loadFilterTree);
 
