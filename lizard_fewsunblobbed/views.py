a427b22356f6141ce937105d314fb44c37a201d2
jack.ha <jack.ha@476a50c0-da5d-5340-8c75-4ad663a144f1>
2011-04-21 09:05:32 +0000

    removed unused imports

--- lizard_fewsunblobbed/views.py
+++ lizard_fewsunblobbed/views.py
@@ -3,16 +3,12 @@ import logging
 from django.conf import settings
 from django.core.cache import cache
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from lizard_fewsunblobbed.models import Filter
 from lizard_fewsunblobbed.models import Parameter
-from lizard_map.daterange import current_start_end_dates
-from lizard_map.daterange import DateRangeForm
-from lizard_map.workspace import WorkspaceManager
 
 FILTER_CACHE_KEY = 'lizard.fewsunblobbed.views.filter_cache_key'
 logger = logging.getLogger(__name__)
