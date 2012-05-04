d61868dfab04ef86839bc229ad9d0b0dc258693a
erikjan.vos <erikjan.vos@476a50c0-da5d-5340-8c75-4ad663a144f1>
2012-05-04 12:56:31 +0000

    fixed import.sql

--- import.sql
+++ import.sql
@@ -5,7 +5,7 @@
 .import parameter.dump parameter
 .import timeserie.dump timeserie
 .import timeseriedata.dump timeseriedata
-UPDATE filter SET parentfkey= NULL WHERE parentfkey='\N'
-UPDATE location SET parentid= NULL WHERE parentid='\N'
-UPDATE location SET tooltiptext= NULL WHERE tooltiptext='\N'
-UPDATE timeseriedata SET tsd_comments= NULL WHERE tsd_comments='\N'
+UPDATE filter SET parentfkey=NULL WHERE parentfkey='\N';
+UPDATE location SET parentid=NULL WHERE parentid='\N';
+UPDATE location SET tooltiptext=NULL WHERE tooltiptext='\N';
+UPDATE timeseriedata SET tsd_comments=NULL WHERE tsd_comments='\N';
