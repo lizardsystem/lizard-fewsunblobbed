e8dcd1e1eb624fc6541b96411b866d3881a2c25e
erikjan.vos <erikjan.vos@476a50c0-da5d-5340-8c75-4ad663a144f1>
2012-05-04 11:53:08 +0000

    updated urls for new lizard-ui

--- lizard_fewsunblobbed/urls.py
+++ lizard_fewsunblobbed/urls.py
@@ -4,23 +4,12 @@ from django.contrib import admin
 
 admin.autodiscover()
 
+from lizard_ui.urls import debugmode_urlpatterns
 from lizard_fewsunblobbed.views import FewsBrowserView
 
 urlpatterns = patterns(
     '',
-    url(r'^$',
-        FewsBrowserView.as_view(),
-        name="fews_browser",
-        ),
+    url(r'^$', FewsBrowserView.as_view(), name="fews_browser"),
     (r'^map/', include('lizard_map.urls')),
-    )
-
-
-if settings.DEBUG:
-    urlpatterns += patterns(
-        '',
-        (r'', include('staticfiles.urls')),
-        (r'^map/', include('lizard_map.urls')),
-        (r'^ui/', include('lizard_ui.urls')),
-        (r'^admin/', include(admin.site.urls)),
-    )
+)
+urlpatterns += debugmode_urlpatterns()
